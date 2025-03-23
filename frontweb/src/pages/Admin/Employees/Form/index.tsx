7c3ff05dfa421efed4e1e74db5f492afb45535b3
Helder Andrey <helderargomes@gmail.com>
2025-03-23 13:06:48 -0300

    Form setup

--- frontweb/src/pages/Admin/Employees/Form/index.tsx
+++ frontweb/src/pages/Admin/Employees/Form/index.tsx
@@ -1,41 +1,117 @@
 import { useHistory } from 'react-router-dom';
 import './styles.css';
+import { Department } from 'types/department';
+import { Controller, useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { requestBackend } from 'util/requests';
+import Select from 'react-select';
+import { AxiosRequestConfig } from 'axios';
+
+
+type FormData = {
+  name: string;
+  email: string;
+  department: Department;  
+}
 
 const Form = () => {
 
+  const [selectDepartments, setSelectDepartments] = useState<Department[]>([]);
+  
   const history = useHistory();
 
   const handleCancel = () => {
     history.push("/admin/employees");
   };
 
+  const { register, handleSubmit, formState: { errors }, control } = useForm<FormData>();
+
+  useEffect(() => {
+    requestBackend({ url: "/departments", withCredentials: true }).then((response) => {
+     setSelectDepartments(response.data);
+    });
+  }, []);
+
+  const onSubmit = (formData: FormData) => {
+    const data = {
+      ...formData,
+      department : {
+        id: formData.department.id,
+        name: null
+      }
+    };
+
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: "/employees",
+      data,
+      withCredentials: true,
+    };
+    requestBackend(config).then((response) => {
+      console.log(response);
+    });
+  }  
+
   return (
     <div className="employee-crud-container">
       <div className="base-card employee-crud-form-card">
         <h1 className="employee-crud-form-title">INFORME OS DADOS</h1>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row employee-crud-inputs-container">
             <div className="col employee-crud-inputs-left-container">
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input is-invalid"
+                <input 
+                {...register("name", {
+                  required: "Campo obrigatório",
+                })}
+                type="text" 
+                  className={`form-control base-input ${errors.name ? "is-invalid" : ""}`}
+                  placeholder="Nome do funcionário"
+                  name="name"
                 />
                 <div className="invalid-feedback d-block">
-                  Mensagem de erro
+                  {errors.name?.message}
                 </div>
               </div>
 
               <div className="margin-bottom-30">
-                <input type="text" 
-                  className="form-control base-input"
+                <input 
+                {...register("email", {
+                  required: "Campo obrigatório",
+                })}
+                type="text" 
+                  className={`form-control base-input ${errors.email ? "is-invalid" : ""}`}
+                  placeholder="E-mail do funcionário"
+                  name="email"
                 />
                 <div className="invalid-feedback d-block">
-                  
+                  {errors.email?.message}
                 </div>
               </div>
 
+              <div className="margin-bottom-30">
+                <Controller 
+                  name="department"
+                  rules={{ required: true }}
+                  control={control}
+                  render={({ field }) => (
+                    <Select 
+                      {...field}
+                      options={selectDepartments}
+                      isClearable
+                      classNamePrefix="department-select"
+                      getOptionLabel={(department: Department) => department.name}
+                      getOptionValue={(department:Department) => String(department.id)}
+                    />
+                      )}/>
+                {errors.department && (
+                  <div className="invalid-feedback d-block">
+                  Campo obrigatório
+                </div>
+                  )}                
+              </div>
             </div>
           </div>
           <div className="employee-crud-buttons-container">
